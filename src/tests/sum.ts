8485b228ef2b34a260039670ea80106a2ca8520a
AritanaPianco <aritanapianco10@gmail.com>
2025-02-21 18:07:33 -0300

    feat: adding vitest

---
+++ src/tests/sum.ts
@@ -0,0 +1,6 @@
+
+export function sum(a: number, b: number) {
+    return a + b
+  }
+
+  sum(1,2)
---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vitest/config'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+    
+  test: {
+     globals: true,
+     include: ["src/**/*.{test,spec}.?(c|m)[jt]s?(x)"],
+  },
+})
