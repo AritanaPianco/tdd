ca71c7dc8f9c7ef3fae08da2303bb7d70cdffe6c
AritanaPianco <aritanapianco10@gmail.com>
2025-02-21 16:00:58 -0300

    feat: add some scripts

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,4 +1,13 @@
 import { Processo } from '@/pasta';
 
-const p1 = new Processo('2');
-console.log(p1);
+const p1 = new Processo('2')
+console.log(p1)
+
+
+const p2 = new Processo('2')
+console.log(p2)
+
+
+
+
+
