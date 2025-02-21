d87464caa34952083e845d6ab62deb3d2cd31276
AritanaPianco <aritanapianco10@gmail.com>
2025-02-21 19:50:51 -0300

    feat: add types of errors with status code

---
+++ src/presentation/helpers/http-response.ts
@@ -0,0 +1,30 @@
+import { ServerError, UnauthorizedError } from '../errors';
+import type { HttpResponse } from '../protocols/http-response';
+
+export const badRequest = (error: Error): HttpResponse => {
+  return {
+    statusCode: 400,
+    body: error,
+  };
+};
+
+export const unauthorizedError = (): HttpResponse => {
+  return {
+    statusCode: 401,
+    body: new UnauthorizedError(),
+  };
+};
+
+export const serverError = (): HttpResponse => {
+  return {
+    statusCode: 500,
+    body: new ServerError(),
+  };
+};
+
+export const ok = (data: any): HttpResponse => {
+  return {
+    statusCode: 200,
+    body: data,
+  };
+};
