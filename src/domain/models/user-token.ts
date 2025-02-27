f95e8536451bc459a3eeb35c1a3a2f053e86ca39
AritanaPianco <aritanapianco10@gmail.com>
2025-02-26 23:37:56 -0300

    refactor: refactor model UserToken to class

--- src/domain/models/user-token.ts
+++ src/domain/models/user-token.ts
@@ -1,5 +1,32 @@
-export interface UserToken {
-  id: string;
+import { v4 as uuidv4 } from 'uuid';
+
+export interface UserTokenProps {
   userId: string;
   token: string;
+}
+
+export class UserToken {
+  constructor(
+    private readonly _id: string,
+    private readonly _userId: string,
+    private readonly _token: string,
+  ) {}
+
+  get id() {
+    return this._id;
+  }
+
+  get userId() {
+    return this._userId;
+  }
+
+  get token() {
+    return this._token;
+  }
+
+  static create({ userId, token }: UserTokenProps) {
+    const id = uuidv4();
+    const userToken = new UserToken(id, userId, token);
+    return userToken;
+  }
 }
