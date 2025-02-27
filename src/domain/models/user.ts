d166fc49902d9224a4ef40dca08e26dedd5aeb81
AritanaPianco <aritanapianco10@gmail.com>
2025-02-26 23:37:33 -0300

    refactor: refactor model User to a class

--- src/domain/models/user.ts
+++ src/domain/models/user.ts
@@ -1,5 +1,40 @@
-export interface User {
+import { v4 as uuidv4 } from 'uuid';
+
+export interface UserProps {
   id: string;
+  name: string;
   email: string;
   password: string;
+}
+
+export class User {
+  constructor(
+    private readonly _id: string,
+    private readonly _name: string,
+    private readonly _email: string,
+    private readonly _password: string,
+  ) {}
+
+  get id() {
+    return this._id;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get email() {
+    return this._email;
+  }
+
+  get password() {
+    return this._password;
+  }
+
+  static create({ name, email, password }: UserProps) {
+    const id = uuidv4();
+    const user = new User(id, name, email, password);
+
+    return user;
+  }
 }
