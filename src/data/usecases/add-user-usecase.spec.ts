d46b34d13cdc497e77cc9dd1d4d00d9b237c6772
AritanaPianco <aritanapianco10@gmail.com>
2025-03-01 18:56:12 -0300

    test: ensure AddUser UseCase calls UsersRepository with correct email

--- src/data/usecases/add-user-usecase.spec.ts
+++ src/data/usecases/add-user-usecase.spec.ts
@@ -1,5 +1,22 @@
 import type { Hash } from '@/domain/cryptography/hash';
+import type { User } from '@/domain/models/user';
+import type { UserRepository } from '@/domain/repositories/user-repository';
 import { AddUser } from './add-user-usecase';
+
+const makeUserRepository = (): UserRepository => {
+  class UserRepositoryStub implements UserRepository {
+    async loadByEmail(email: string): Promise<User | null> {
+      const user = {
+        id: 'any_id',
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'hashed_password',
+      };
+      return user as User;
+    }
+  }
+  return new UserRepositoryStub();
+};
 
 const makeHashStub = (): Hash => {
   class HashStub implements Hash {
@@ -13,14 +30,17 @@ const makeHashStub = (): Hash => {
 interface SutTypes {
   sut: AddUser;
   hashStub: Hash;
+  usersRepositoryStub: UserRepository;
 }
 
 const makeSut = (): SutTypes => {
+  const usersRepositoryStub = makeUserRepository();
   const hashStub = makeHashStub();
-  const sut = new AddUser(hashStub);
+  const sut = new AddUser(usersRepositoryStub, hashStub);
   return {
     sut,
     hashStub,
+    usersRepositoryStub,
   };
 };
 
@@ -35,5 +55,16 @@ describe('AddUser UseCase', () => {
     const hashSpy = vi.spyOn(hashStub, 'hash');
     await sut.execute(user);
     expect(hashSpy).toHaveBeenCalledWith(user.password, 8);
+  });
+  test('should call UsersRepository with correct email', async () => {
+    const { sut, usersRepositoryStub } = makeSut();
+    const user = {
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    };
+    const loadByEmailSpy = vi.spyOn(usersRepositoryStub, 'loadByEmail');
+    await sut.execute(user);
+    expect(loadByEmailSpy).toHaveBeenCalledWith(user.email);
   });
 });
