e25ccc4b9c2c245a778c039f43e404ed6dba8810
AritanaPianco <aritanapianco10@gmail.com>
2025-03-01 18:55:38 -0300

    feat: ensure AddUser UseCase calls UsersRepository with correct email

--- src/data/usecases/add-user-usecase.ts
+++ src/data/usecases/add-user-usecase.ts
@@ -1,17 +1,22 @@
 import type { Hash } from '@/domain/cryptography/hash';
 import { User, type UserProps } from '@/domain/models/user';
+import type { UserRepository } from '@/domain/repositories/user-repository';
 import type { AddUserUseCase } from '@/domain/usecases/add-user-usecase';
 
 export class AddUser implements AddUserUseCase {
-  constructor(private readonly hash: Hash) {}
+  constructor(
+    private readonly usersRepository: UserRepository,
+    private readonly hash: Hash,
+  ) {}
 
   async execute(user: UserProps): Promise<void> {
+    await this.usersRepository.loadByEmail(user.email);
+
     const hashedPassword = await this.hash.hash(user.password, 8);
     const newUser = User.create({
       name: user.name,
       email: user.email,
       password: hashedPassword,
     });
-    return hashedPassword;
   }
 }
