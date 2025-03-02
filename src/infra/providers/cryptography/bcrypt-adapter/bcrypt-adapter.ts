dba070faf15f9f2666b65f2271a28f877f581fe7
AritanaPianco <aritanapianco10@gmail.com>
2025-03-01 21:00:41 -0300

    feat: ensure BcryptAdapter calls hash with corrects values

--- src/infra/providers/cryptography/bcrypt-adapter/bcrypt-adapter.ts
+++ src/infra/providers/cryptography/bcrypt-adapter/bcrypt-adapter.ts
@@ -1,7 +1,12 @@
+import type { Hash } from '@/domain/cryptography/hash';
 import type { HashComparer } from '@/domain/cryptography/hash-comparer';
 import bcrypt from 'bcrypt';
 
-export class BcryptAdapter implements HashComparer {
+export class BcryptAdapter implements HashComparer, Hash {
+  async hash(value: string, salt: number): Promise<string> {
+    const hashedValue = bcrypt.hash(value, salt);
+    return hashedValue;
+  }
   async compare(value: string, hash: string): Promise<boolean> {
     return bcrypt.compare(value, hash);
   }
