bb8c0696fc5c6e917b21d70e8f90c3b87010bbd9
AritanaPianco <aritanapianco10@gmail.com>
2025-03-03 20:44:31 -0300

    feat: ensure AuthMiddlewareUseCase returns 200 if loadUserByTokenUseCase returns an user

--- src/presentation/middleware/auth-middleware.spec.ts
+++ src/presentation/middleware/auth-middleware.spec.ts
@@ -1,13 +1,19 @@
 import type { User } from '@/domain/models/user';
 import type { LoadUserByToken } from '@/domain/usecases/load-user-by-token';
 import { AccessDeniedError } from '../errors';
-import { forbiddenError } from '../helpers';
+import { forbiddenError, ok } from '../helpers';
 import { AuthMiddleware } from './auth-middleware';
 
 const makeLoadUserByToken = (): LoadUserByToken => {
   class LoadUserByTokenUseCaseStub implements LoadUserByToken {
     async execute(token: string): Promise<User | null> {
-      return new Promise((resolve) => resolve(null));
+      const fakerUser = {
+        id: 'valid_id',
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+      };
+      return new Promise((resolve) => resolve(fakerUser as User));
     }
   }
   return new LoadUserByTokenUseCaseStub();
@@ -63,5 +69,17 @@ describe('AuthMiddleware', () => {
     expect(response.statusCode).toBe(403);
     expect(response.body).toEqual(new AccessDeniedError());
     expect(response).toEqual(forbiddenError(new AccessDeniedError()));
+  });
+  test('should return 200 if loadUserByTokenUseCase returns an user', async () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      headers: {
+        authorization: 'any_token',
+      },
+    };
+    const response = await sut.handle(httpRequest);
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({ userId: 'valid_id' });
+    expect(response).toEqual(ok({ userId: 'valid_id' }));
   });
 });
--- src/presentation/middleware/auth-middleware.ts
+++ src/presentation/middleware/auth-middleware.ts
@@ -1,25 +1,20 @@
 import type { LoadUserByToken } from '@/domain/usecases/load-user-by-token';
 import { AccessDeniedError } from '../errors';
-import { forbiddenError } from '../helpers';
+import { forbiddenError, ok } from '../helpers';
 import type { HttpRequest, HttpResponse, Middleware } from '../protocols';
 
 export class AuthMiddleware implements Middleware {
-  private reponse: HttpResponse = {
-    statusCode: 0,
-    body: undefined,
-  };
-
   constructor(private readonly loadUserByToken: LoadUserByToken) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const accessToken = httpRequest.headers['authorization'];
     if (!accessToken) {
-      this.reponse = forbiddenError(new AccessDeniedError());
+      return forbiddenError(new AccessDeniedError());
     }
     const user = await this.loadUserByToken.execute(accessToken);
     if (!user) {
-      this.reponse = forbiddenError(new AccessDeniedError());
+      return forbiddenError(new AccessDeniedError());
     }
-    return this.reponse;
+    return ok({ userId: user?.id });
   }
 }
