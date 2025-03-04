dce356c68a71a68d88e015e1aa9566a3338161b2
AritanaPianco <aritanapianco10@gmail.com>
2025-03-03 22:18:00 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/load-user-by-token-usecase.spec.ts
+++ src/data/usecases/load-user-by-token-usecase.spec.ts
@@ -1,31 +1,43 @@
 import type { Decrypter } from '@/domain/cryptography/decrypter';
 import { LoadUserByTokenUseCase } from './load-user-by-token-usecase';
 
+const makeDecrypter = (): Decrypter => {
+  class DecrypterStub implements Decrypter {
+    async decrypt(value: string): Promise<string> {
+      return new Promise((resolve) => resolve('decoded_value'));
+    }
+  }
+
+  return new DecrypterStub();
+};
+
+interface SutTypes {
+  sut: LoadUserByTokenUseCase;
+  decrypterStub: Decrypter;
+}
+
+const makeSut = (): SutTypes => {
+  const decrypterStub = makeDecrypter();
+  const sut = new LoadUserByTokenUseCase(decrypterStub);
+  return {
+    sut,
+    decrypterStub,
+  };
+};
+
 describe('LoadUserByToken UseCase', () => {
   test('should call Decrypter with correct token', async () => {
-    class DecrypterStub implements Decrypter {
-      async decrypt(value: string): Promise<string> {
-        return new Promise((resolve) => resolve('decoded_value'));
-      }
-    }
-    const decrypterStub = new DecrypterStub();
+    const { sut, decrypterStub } = makeSut();
     const decryptSpy = vi.spyOn(decrypterStub, 'decrypt');
-    const sut = new LoadUserByTokenUseCase(decrypterStub);
     const token = 'any_token';
     await sut.execute(token);
     expect(decryptSpy).toHaveBeenCalledWith('any_token');
   });
   test('should return null if an invalid token is provided', async () => {
-    class DecrypterStub implements Decrypter {
-      async decrypt(value: string): Promise<string> {
-        return new Promise((resolve) => resolve('decoded_value'));
-      }
-    }
-    const decrypterStub = new DecrypterStub();
+    const { sut, decrypterStub } = makeSut();
     vi.spyOn(decrypterStub, 'decrypt').mockReturnValueOnce(
       new Promise((resolve) => resolve(null!)),
     );
-    const sut = new LoadUserByTokenUseCase(decrypterStub);
     const token = 'any_token';
     const response = await sut.execute(token);
     expect(response).toBeNull();
