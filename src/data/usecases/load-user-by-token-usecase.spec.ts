cfef8ff26950f3f583772675bfdc04441f732171
AritanaPianco <aritanapianco10@gmail.com>
2025-03-03 22:27:37 -0300

    test: ensure LoadUserByTokenUseCase calls usersTokenRepository with correct token

--- src/data/usecases/load-user-by-token-usecase.spec.ts
+++ src/data/usecases/load-user-by-token-usecase.spec.ts
@@ -1,4 +1,7 @@
 import type { Decrypter } from '@/domain/cryptography/decrypter';
+import type { User } from '@/domain/models/user';
+import type { UserToken } from '@/domain/models/user-token';
+import type { UserTokenRepository } from '@/domain/repositories/user-token-repository';
 import { LoadUserByTokenUseCase } from './load-user-by-token-usecase';
 
 const makeDecrypter = (): Decrypter => {
@@ -11,17 +14,41 @@ const makeDecrypter = (): Decrypter => {
   return new DecrypterStub();
 };
 
+const makeUsersTokensRepository = (): UserTokenRepository => {
+  class UsersTokensRepositoryStub implements UserTokenRepository {
+    private usersToken: UserToken[] = [];
+
+    async findByToken(token: string): Promise<UserToken> {
+      const userToken = this.usersToken.filter(
+        (userToken) => userToken.token === token,
+      );
+      return userToken[0];
+    }
+
+    async create(data: User, token: string): Promise<void> {}
+
+    async updateAccessToken(userId: string, token: string): Promise<void> {}
+  }
+  return new UsersTokensRepositoryStub();
+};
+
 interface SutTypes {
   sut: LoadUserByTokenUseCase;
   decrypterStub: Decrypter;
+  usersTokensRepositoryStub: UserTokenRepository;
 }
 
 const makeSut = (): SutTypes => {
   const decrypterStub = makeDecrypter();
-  const sut = new LoadUserByTokenUseCase(decrypterStub);
+  const usersTokensRepositoryStub = makeUsersTokensRepository();
+  const sut = new LoadUserByTokenUseCase(
+    decrypterStub,
+    usersTokensRepositoryStub,
+  );
   return {
     sut,
     decrypterStub,
+    usersTokensRepositoryStub,
   };
 };
 
@@ -41,5 +68,12 @@ describe('LoadUserByToken UseCase', () => {
     const token = 'any_token';
     const response = await sut.execute(token);
     expect(response).toBeNull();
+  });
+  test('should call usersTokenRepository with correct token', async () => {
+    const { sut, usersTokensRepositoryStub } = makeSut();
+    const findByTokenSpy = vi.spyOn(usersTokensRepositoryStub, 'findByToken');
+    const token = 'any_token';
+    await sut.execute(token);
+    expect(findByTokenSpy).toHaveBeenCalledWith('any_token');
   });
 });
--- src/data/usecases/load-user-by-token-usecase.ts
+++ src/data/usecases/load-user-by-token-usecase.ts
@@ -1,14 +1,19 @@
 import type { Decrypter } from '@/domain/cryptography/decrypter';
 import type { User } from '@/domain/models/user';
+import type { UserTokenRepository } from '@/domain/repositories/user-token-repository';
 import type { LoadUserByToken } from '@/domain/usecases/load-user-by-token';
 
 export class LoadUserByTokenUseCase implements LoadUserByToken {
-  constructor(private readonly decrypter: Decrypter) {}
+  constructor(
+    private readonly decrypter: Decrypter,
+    private readonly usersTokensRepository: UserTokenRepository,
+  ) {}
 
   async execute(token: string): Promise<User | null> {
     const decodedValue = await this.decrypter.decrypt(token);
     if (!decodedValue) {
       return null;
     }
+    await this.usersTokensRepository.findByToken(token);
   }
 }
